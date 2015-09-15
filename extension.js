80f4844fcd3fa57b19bc27d4aa44fc8c2e312851
MerileStudiosOfficial <sonichedgehog1@hotmail.com>
2015-09-14 20:40:57 -0500

    Update extension.js

--- extension.js
+++ extension.js
@@ -141,6 +141,19 @@
                 }
               }
             };
+            
+        bot.commands.helpCommand = {
+            command: 'commands',  // The command to be called. With the standard command literal this would be: !bacon
+            rank: 'user', // Minimum user permission to use the command
+            type: 'exact', // Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+              functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                  API.sendChat("/me !leone, !ryuko, !icsp, !tails, !aboos, !kappa, !derp, !grill, !killlatails ");
+                }
+              }
+            };
 
         bot.commands.kteCommand = {
             command: 'derp',  // The command to be called. With the standard command literal this would be: !bacon
