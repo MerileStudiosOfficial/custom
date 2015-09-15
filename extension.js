c4c217042158296f1a7225cdc633d8379e6bc2c6
MerileStudiosOfficial <sonichedgehog1@hotmail.com>
2015-09-14 20:13:34 -0500

    f
    a

--- extension.js
+++ extension.js
@@ -142,6 +142,19 @@
               }
             };
 
+        bot.commands.kteCommand = {
+            command: 'derp',  // The command to be called. With the standard command literal this would be: !bacon
+            rank: 'user', // Minimum user permission to use the command
+            type: 'exact', // Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+              functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                  API.sendChat("https://lh3.googleusercontent.com/-l047245cEBI/VaqPkYP72gI/AAAAAAAAA3c/aEHApEu6KQk/w640-h480/standalone.gif");
+                }
+              }
+            };
+
         // Load the chat package again to account for any changes
         bot.loadChat();
 
